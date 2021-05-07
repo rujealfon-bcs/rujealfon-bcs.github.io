b7f608cdaf80559411d1d384ad376346364f0d14
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2021-05-07 23:06:56 +0800

    test

---
+++ _nuxt/static/1620399981/async-data/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620399981",layout:"default",error:a,serverRendered:true,routePath:"\u002Fasync-data",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b}}}}(null,"\u002F"));
